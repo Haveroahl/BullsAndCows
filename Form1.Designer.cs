ba136527fc78eef5ac6b9d9a1ad8055ee6e3cd97
Minh Đức <118919625+Haveroahl@users.noreply.github.com>
2025-01-29 21:15:11 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.textBoxGuess = new System.Windows.Forms.TextBox();
+            this.buttonGuess = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(12, 9);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(209, 16);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = "Thử đoán 1 số có đủ 4 chữ số nào.";
+            // 
+            // textBoxGuess
+            // 
+            this.textBoxGuess.Location = new System.Drawing.Point(12, 47);
+            this.textBoxGuess.Name = "textBoxGuess";
+            this.textBoxGuess.Size = new System.Drawing.Size(101, 22);
+            this.textBoxGuess.TabIndex = 1;
+            this.textBoxGuess.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxGuess_KeyPress);
+            // 
+            // buttonGuess
+            // 
+            this.buttonGuess.Location = new System.Drawing.Point(136, 47);
+            this.buttonGuess.Name = "buttonGuess";
+            this.buttonGuess.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuess.TabIndex = 2;
+            this.buttonGuess.Text = "Đoán";
+            this.buttonGuess.UseVisualStyleBackColor = true;
+            this.buttonGuess.Click += new System.EventHandler(this.buttonGuess_Click);
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.ItemHeight = 16;
+            this.listBoxResults.Location = new System.Drawing.Point(231, 36);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(198, 276);
+            this.listBoxResults.TabIndex = 3;
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(12, 90);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(199, 49);
+            this.buttonRestart.TabIndex = 4;
+            this.buttonRestart.Text = "Chơi lại";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonGuess;
+            this.ClientSize = new System.Drawing.Size(446, 323);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.buttonGuess);
+            this.Controls.Add(this.textBoxGuess);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "Form1";
+            this.Text = "Bulls and Cows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.TextBox textBoxGuess;
+        private System.Windows.Forms.Button buttonGuess;
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Button buttonRestart;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        private string secretNumber;
+        private int attempts;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            secretNumber = GenerateSecretNumber();
+            attempts = 0;
+            listBoxResults.Items.Clear();
+            labelMessage.Text = "Thử đoán 1 số có đủ 4 chữ số nào.";
+            buttonGuess.Enabled = true;
+        }
+
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            string guess = textBoxGuess.Text;
+            attempts++;
+
+            if (guess.Length != 4 || !IsAllDigits(guess))
+            {
+                labelMessage.Text = "Nhập sai. Vui lòng nhập số có đủ 4 chữ số: ";
+                return;
+            }
+
+            (int bulls, int cows) = GetBullsAndCows(secretNumber, guess);
+
+            if (bulls == 4)
+            {
+                labelMessage.Text = $"Chúc mừng! Bạn đã đoán đúng con số {secretNumber} trong {attempts} lượt đoán.";
+                buttonGuess.Enabled = false;
+            }
+            else
+            {
+                listBoxResults.Items.Add($"{guess}: {bulls} Bulls và {cows} Cows");
+                labelMessage.Text = "Thử lại!";
+            }
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e)
+        {
+            InitializeGame();
+        }
+
+        private string GenerateSecretNumber()
+        {
+            Random random = new Random();
+            char[] digits = new char[4];
+            int i = 0;
+
+            while (i < 4)
+            {
+                char digit = (char)('0' + random.Next(0, 10));
+                if (!ArrayContains(digits, digit))
+                {
+                    digits[i] = digit;
+                    i++;
+                }
+            }
+
+            return new string(digits);
+        }
+
+        private bool ArrayContains(char[] array, char value)
+        {
+            foreach (char item in array)
+            {
+                if (item == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsAllDigits(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private (int bulls, int cows) GetBullsAndCows(string secret, string guess)
+        {
+            int bulls = 0;
+            int cows = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (secret[i] == guess[i])
+                {
+                    bulls++;
+                }
+                else if (CharArrayContains(secret, guess[i]))
+                {
+                    cows++;
+                }
+            }
+
+            return (bulls, cows);
+        }
+
+        private bool CharArrayContains(string str, char value)
+        {
+            foreach (char c in str)
+            {
+                if (c == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void textBoxGuess_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only numeric characters and control characters (e.g., backspace)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
